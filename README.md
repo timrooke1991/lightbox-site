c3b73505d3130282a0420737b3d17946ee1a871c
Tim Rooke <timrooke1991@gmail.com>
2017-04-13 17:42:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# rns_project
---
+++ js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+	$('nav a').on('click', function() {
+		// Current class assignment 
+		$('nav li.current').removeClass('current');
+		$(this).parent().addClass('current');
+
+		// Set heading text to the right label
+		$('h1#heading').text($(this).text());
+
+		// Get & Filter link text
+		var category = $(this).text().toLowerCase().replace(' ', '-');
+
+		// Remove hidden class if 'all-projects' is selected
+		if (category === 'all-projects') {
+			$('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
+		} else {
+			$('ul#gallery li').each(function() { 
+				if(!$(this).hasClass(category)) {
+					$(this).hide().addClass('hidden');
+				} else {
+					$(this).fadeIn('slow').removeClass('hidden');
+				}
+			});
+		}
+		// Stop link behaviour
+		return false
+
+
+	});
+
+	//Mouseenter overlay
+	$('ul#gallery li').on('mouseenter', function() {
+		// Get data attribute values
+		var title = $(this).children().data('title');
+		var desc = $(this).children().data('desc');
+
+		// Validation
+		if(desc === null) {
+			desc = 'Click To Enlarge';
+		}
+
+		if (title === null) { 
+			title = ''; 
+		}
+
+		// Create overlay div
+		$(this).append('<div class="overlay"></div>');
+
+		// Get the overlay div
+		var overlay = $(this).children('.overlay');
+
+		// Add HTML to overlay
+		overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+		// Fade in overlay
+		overlay.fadeIn(800);
+	});
+
+	// Mouseleave overlay
+	$('ul#gallery li').on('mouseleave', function() {
+		// Create overlay div
+		$(this).append('<div class="overlay"></div>');
+
+		// Get the overlay div
+		var overlay = $(this).children('.overlay');
+
+		// Fade out overlay
+		overlay.fadeOut();
+	})
+
+});
